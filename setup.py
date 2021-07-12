4bb3196916e1cede6017c86f02e23e66f73704f7
Pitchblack408 <8700224+pitchblack408@users.noreply.github.com>
2021-07-12 15:48:34 -0700

    changing version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyawscron-pitchblack408",
-    version="0.0.1",
+    version="0.0.2",
     author="Michael Martin",
     author_email="pitchblack408@gmail.com",
     description="An AWS Cron Parser",
