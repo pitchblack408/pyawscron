ba82ec927342ee96b9f7e47d3228c4a4c93d7d11
Tom Fleet <tomfleet2018@gmail.com>
2022-05-12 17:51:15 +0100

    Fix undeclared dependency on python-dateutil

--- setup.py
+++ setup.py
@@ -1,37 +1,28 @@
-import setuptools
-import os
-
-
-version = {}
-with open(os.path.abspath("src"+os.path.sep+"pyawscron"+os.path.sep+"version.py")) as fp:
-    exec(fp.read(), version)
-
-try:
-    assert "." in version['__version__']
-except AssertionError:
-    raise AssertionError("Failed to obtain version.")
+from setuptools import find_packages, setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="pyawscron",
-    version=version['__version__'],
-    author="Michael Martin",
-    author_email="pitchblack408@gmail.com",
-    description="An AWS Cron Parser",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pitchblack408/pyawscron",
-    project_urls={
-        "Bug Tracker": "https://github.com/pitchblack408/pyawscron/issues",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-    ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
-)
+if __name__ == "__main__":
+    setup(
+        name="pyawscron",
+        version="1.0.4",
+        author="Michael Martin",
+        author_email="pitchblack408@gmail.com",
+        description="An AWS Cron Parser",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/pitchblack408/pyawscron",
+        project_urls={
+            "Bug Tracker": "https://github.com/pitchblack408/pyawscron/issues",
+        },
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: BSD License",
+            "Operating System :: OS Independent",
+        ],
+        package_dir={"": "src"},
+        packages=find_packages(where="src"),
+        python_requires=">=3.6",
+        install_requires=["python-dateutil~=2.8.1"],
+    )
